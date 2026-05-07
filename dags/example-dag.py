546ed859869b7f9d2bab9d952b2d37d16e8093f9
mfonekpo <mfonekpo34@gmail.com>
2026-05-07 15:14:24 +0100

    FEAT: updated ruff format on github workflow and commit dags code

--- dags/example-dag.py
+++ dags/example-dag.py
@@ -3,17 +3,19 @@ from airflow.sdk import dag, task
 from etl.ingest import ingest_to_bucket
 import pendulum
 
+
 @task(retries=3)
 def ingestion():
     ingest_to_bucket()
 
 
 @dag(
-    # schedule="@hourly",
-    schedule="* * * * *",
-    start_date=pendulum.now(tz="UTC").subtract(days=1),
+    schedule="@hourly",
+    # schedule="* * * * *",
+    start_date=pendulum.datetime(2026, 1, 1, tz="UTC"),
     catchup=False,
     is_paused_upon_creation=False,
+    max_active_runs=1,
 )
 def taskflow():
     ingestion()
