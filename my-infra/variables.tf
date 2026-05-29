5ee234e2254467fe3c933f4003fd3d89735e8dfc
mfonekpo <mfonekpo34@gmail.com>
2026-05-29 14:24:17 +0100

    FEAT: Updated my infra and included sqs trigger feature

--- my-infra/variables.tf
+++ my-infra/variables.tf
@@ -5,15 +5,20 @@ variable "aws_region" {
 }
 
 variable "s3_buckets" {
-    description = "Map of S3 buckets to create"
-    type        = map(object({
-        name = string
-        environment = string  
-    }))
+  description = "Map of S3 buckets to create"
+  type = map(object({
+    name        = string
+    environment = string
+  }))
 }
 
 variable "environment" {
-    description = "The environment to deploy resources in."
-    type        = string
-    default     = "dev"
+  description = "The environment to deploy resources in."
+  type        = string
+  default     = "dev"
+}
+
+variable "queue_name" {
+  description = "The name of the SQS queue."
+  type        = string
 }
