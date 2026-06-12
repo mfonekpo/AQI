63df7d58e074dfce21e563b6cbf3fdccd6c65dd1
mfonekpo <mfonekpo34@gmail.com>
2026-06-12 17:49:58 +0100

    hotfix: decoupled dag to producer and consumer

---
+++ dags/producer-dag.py
@@ -0,0 +1,41 @@
+from airflow.sdk import dag, task
+from alerting.alert import send_telegram_alert
+from etl.ingest import ingest_to_bucket
+import pendulum
+
+
+def on_failure_callback(context: dict) -> None:
+    dag_id  = context["dag"].dag_id
+    task_id = context["task_instance"].task_id
+    run_id  = context["run_id"]
+    send_telegram_alert(
+        f"❌ Pipeline failed!\n"
+        f"DAG:  {dag_id}\n"
+        f"Task: {task_id}\n"
+        f"Run:  {run_id}"
+    )
+
+@task()
+def ingestion():
+    ingest_to_bucket()
+
+@dag(
+    dag_id="aqi_producer_dag",
+    schedule="@hourly",
+    start_date=pendulum.datetime(2026, 5, 28, tz="UTC"),
+    catchup=False,
+    is_paused_upon_creation=False,
+    max_active_runs=3,
+    on_failure_callback=on_failure_callback,
+    default_args={
+        "retries": 2,
+        "retry_delay": pendulum.duration(seconds=120)
+    },
+)
+
+def taskflow():
+    ingestion()
+
+
+# Instantiate the DAG
+taskflow()
