646ae642ee1c37e44088064db7208f30aa011fed
mfonekpo <mfonekpo34@gmail.com>
2026-06-15 01:24:37 +0100

    infra: provisioned snowflake infra

--- my-infra/providers.tf
+++ my-infra/providers.tf
@@ -24,10 +24,11 @@ locals {
 
 # configure snowflake provider with private key authentication
 provider "snowflake" {
-  organization_name = var.snowflake_organization_name
-  account_name      = var.snowflake_account_name
-  user              = var.snowflake_user
-  role              = var.snowflake_role
-  authenticator     = "SNOWFLAKE_JWT"
-  private_key       = file(local.private_key_path)
+  organization_name        = var.snowflake_organization_name
+  account_name             = var.snowflake_account_name
+  user                     = var.snowflake_user
+  role                     = var.snowflake_role
+  authenticator            = "SNOWFLAKE_JWT"
+  private_key              = file(local.private_key_path)
+  preview_features_enabled = ["snowflake_storage_integration_aws_resource"]
 }
