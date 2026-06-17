3f6ad857aefbd26e395c0251d5fe0e99bd43c14e
mfonekpo <mfonekpo34@gmail.com>
2026-06-17 04:13:01 +0100

    myinfra:refactor: refatored code to solve snowflake s3 policy error

--- my-infra/iam.tf
+++ my-infra/iam.tf
@@ -8,13 +8,55 @@ resource "aws_iam_role" "snowflake_storage_role" {
         Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
-          AWS = var.snowflake_iam_user_arn
+          AWS = "*"
+          # AWS = snowflake_storage_integration_aws.tf_s3_integration.storage_aws_iam_user_arn
         }
-        Condition = {
-          StringEquals = {
-            "sts:ExternalId" = var.snowflake_external_id
-          }
-        }
+        # Condition = {
+        #   StringEquals = {
+        #     "sts:ExternalId" = snowflake_storage_integration_aws.tf_s3_integration.storage_aws_external_id
+        #   }
+        # }
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy" "snowflake_s3_access" {
+
+  name = "snowflake-s3-access"
+  role = aws_iam_role.snowflake_storage_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+
+      {
+        Sid    = "ListBucket"
+        Effect = "Allow"
+
+        Action = [
+          "s3:ListBucket"
+        ]
+
+        Resource = [
+          aws_s3_bucket.buckets["transform"].arn
+        ]
+      },
+
+      {
+        Sid    = "ReadObjects"
+        Effect = "Allow"
+
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion"
+        ]
+
+        Resource = [
+          "${aws_s3_bucket.buckets["transform"].arn}/*"
+        ]
       }
     ]
   })
